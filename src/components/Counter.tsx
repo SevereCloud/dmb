d1cdf3a525b0fa727fed567023e27ebbc28b08a4
Suvorov Daniil <severecloud@gmail.com>
2021-06-01 10:08:02 +0300

    fix: Counter after dmb

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -10,6 +10,63 @@ export interface CounterProps {
   timer: Timer;
 }
 
+const CounterDaysAfter: FC<{ timer: Timer }> = ({
+  timer,
+}: {
+  timer: Timer;
+}) => {
+  const count = getDays(new Date(timer.end_date * 1000), new Date());
+  return (
+    <>
+      <div className="Counter__description">
+        {numberCase(count, 'Прошел', 'Прошло') + " после дембеля"}
+      </div>
+      <div className="Counter__number">
+        {count}
+        <span className="Counter__unit">
+          {numberCase(count, 'день', 'дня', 'дней')}
+        </span>
+      </div>
+    </>
+  );
+};
+
+const CounterWeeksAfter: FC<{ timer: Timer }> = ({
+  timer,
+}: {
+  timer: Timer;
+}) => {
+  const count = getWeeks(new Date(timer.end_date * 1000), new Date());
+  return (
+    <>
+      <div className="Counter__description">
+        {numberCase(count, 'Прошел', 'Прошло')+ " после дембеля"}
+      </div>
+      <div className="Counter__number">
+        {count}
+        <span className="Counter__unit">
+          {numberCase(count, 'неделя', 'недели', 'недель')}
+        </span>
+      </div>
+    </>
+  );
+};
+
+const CounterPercentAfter: FC<{ timer: Timer }> = () => {
+  return (
+    <>
+      <div className="Counter__description">
+        Прошло после дембеля
+      </div>
+      <div className="Counter__number">
+        100
+        <span className="Counter__unit">%</span>
+      </div>
+    </>
+  );
+};
+
+
 const CounterDaysLeft: FC<{ timer: Timer }> = ({ timer }: { timer: Timer }) => {
   const count = daysLeft(timer.end_date);
   return (
@@ -141,15 +198,17 @@ const Counter: FC<CounterProps> = ({
   updateCounterType,
   timer,
 }: CounterProps) => {
+  const now_time = new Date().getTime() / 1000
   return (
     <div
       onClick={() => {
         let index = counterTypeList.indexOf(counterType) + 1;
+        if (timer.end_date<=now_time) index += 1
         if (index >= counterTypeList.length) index = 0;
         updateCounterType(counterTypeList[index]);
       }}
     >
-      {
+      {timer.end_date>now_time?
         {
           days_left: <CounterDaysLeft timer={timer} />,
           days_passed: <CounterDaysPassed timer={timer} />,
@@ -157,6 +216,13 @@ const Counter: FC<CounterProps> = ({
           weeks_passed: <CounterWeeksPassed timer={timer} />,
           percent_left: <CounterPercentLeft timer={timer} />,
           percent_passed: <CounterPercentPassed timer={timer} />,
+        }[counterType]:{
+          days_left: <CounterDaysAfter timer={timer} />,
+          days_passed: <CounterDaysAfter timer={timer} />,
+          weeks_left: <CounterWeeksAfter timer={timer} />,
+          weeks_passed: <CounterWeeksAfter timer={timer} />,
+          percent_left: <CounterPercentAfter timer={timer} />,
+          percent_passed: <CounterPercentAfter timer={timer} />,
         }[counterType]
       }
     </div>
